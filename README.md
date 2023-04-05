0155f5c227c9adfa476ea81a9261318a9d585df3
reethikar <reethika@umich.edu>
2023-04-05 17:18:42 +0530

    Customize number of chunks in parallel

--- README.md
+++ README.md
@@ -8,9 +8,16 @@ Build the program using `go build main.go`
 
 ## Run 
 By default, the program downloads the recent golang binary release for linux (`https://go.dev/dl/go1.20.3.linux-amd64.tar.gz`).
-But it can take a URL (`--url`) as an input and also an optional `--output` to specify the file path.
+But it can take a URL (`--url`) as an input and also an optional `--output` to specify the file path. The user can also customize the number of chunks to download in parallel using the `-parallel` flag, the default is 10.
 
 
 Running the program:
-- Provide your own URL: `./main --url="https://go.dev/dl/go1.20.3.windows-amd64.zip"`
-- Provide your own URL and desired output file path: `./main --url="https://go.dev/dl/go1.20.3.windows-amd64.zip" --output=downloads/windows-amd64-archive.zip`
+- Provide your own URL: 
+
+  `./main --url="https://go.dev/dl/go1.20.3.windows-amd64.zip"`
+- Provide your own URL and desired output file path: 
+
+  `./main --url="https://go.dev/dl/go1.20.3.windows-amd64.zip" --output=downloads/windows-amd64-archive.zip`
+- Provide your own URL, desired output file path, and desired number of chunks: 
+
+  `./main --url="https://go.dev/dl/go1.20.3.windows-amd64.zip" --output=downloads/windows-amd64-archive.zip --parallel=20`
--- main.go
+++ main.go
@@ -16,13 +16,10 @@ import (
 	"os"
 )
 
-// Define the number of chunks to download by default
-const defaultNumChunks = 10
-
 // confirmSupportAndFileChunkSize tests to see if "Accept-Ranges" is part of the HTTP Response header
 // If HTTP Range requests are not supported, return server not supported error
 // If supported, return the filesize and anticipated chunkSize
-func confirmSupportAndFileChunkSize(dwLink string) (int64, int64, error) {
+func confirmSupportAndFileChunkSize(dwLink string, defaultNumChunks uint) (int64, int64, error) {
 	// Set DisableCompression to true (default is false) 
 	// This ensures Go's internal transport behavior does not mess with our logic
 	tr := &http.Transport{
@@ -39,7 +36,7 @@ func confirmSupportAndFileChunkSize(dwLink string) (int64, int64, error) {
 		return 0, 0, errors.New("Server Error: Accept-Ranges Header does not exist in HTTP Response")
 	}
 	filesize, err := strconv.ParseInt(response.Header["Content-Length"][0], 10, 64)
-	return filesize, (filesize/defaultNumChunks), err
+	return filesize, (filesize/int64(defaultNumChunks)), err
 }
 
 // getDownloadFileName returns the filename of the file hosted at the URL to download
@@ -74,7 +71,7 @@ func getObjectRange(dwLink string, rangeStart int64, rangeEnd int64) (http.Respo
 }
 
 // writeChunks writes the obtained object to the right position in the file
-func writeChunks(response http.Response, fileToWrite *os.File, currChunk int64, rangeStart int64, downloaderWg *sync.WaitGroup) {
+func writeChunks(response http.Response, fileToWrite *os.File, currChunk uint, rangeStart int64, downloaderWg *sync.WaitGroup) {
 	var writeRangeStart = rangeStart
 	// Obtain size of response to compare the bytes read from the object
 	responseSize, _ := strconv.ParseInt(response.Header["Content-Length"][0], 10, 64)
@@ -124,13 +121,15 @@ func isFlagPassed(name string) bool {
 func main() {
 	// Get URL to download and desired output file name
 	var resultFile, dwLink string
+	var defaultNumChunks uint
 	// SHA256 Checksum for https://go.dev/dl/go1.20.3.linux-amd64.tar.gz file from https://go.dev/dl/ is 979694c2c25c735755bf26f4f45e19e64e4811d661dd07b8c010f7a8e18adfca (4/5/23)
 	flag.StringVar(&dwLink, "url", "https://go.dev/dl/go1.20.3.linux-amd64.tar.gz", "URL of the file to download (default: latest go release for linux as of 4/5/23)")
 	flag.StringVar(&resultFile, "output", "", "Path and filename to save output file (default: current directory with filename obtained through the URL)")
+	flag.UintVar(&defaultNumChunks, "parallel", 10, "Number of chunks to download in parallel, type uint (default: 10)")
 	flag.Parse()
 
 	// Check hosting server's support for HTTP Range requests, if yes, get fileSize and anticipated chunkSize
-	fileSize, chunkSize, err := confirmSupportAndFileChunkSize(dwLink)
+	fileSize, chunkSize, err := confirmSupportAndFileChunkSize(dwLink, defaultNumChunks)
 	if err != nil {
 		log.Fatalln("Fatal error in checking support for multi-source downloads: ", err)
 	}
@@ -150,7 +149,7 @@ func main() {
 	var downloaderWg sync.WaitGroup
 	startTime := time.Now()
 	fmt.Println("Downloading ", resultFile, " in ", defaultNumChunks, " chunks...")
-	for i := int64(0); i < defaultNumChunks; i++ {
+	for i := uint(0); i < defaultNumChunks; i++ {
 		if i == defaultNumChunks-1 {
 			// For the last chunk, ensure rangeEnd is up to fileSize
 			rangeEnd = fileSize 
@@ -159,7 +158,7 @@ func main() {
 			rangeEnd = rangeStart + chunkSize - 1 
 		}
 		downloaderWg.Add(1)
-		go func(i int64, dwLink string, rangeStart int64, rangeEnd int64, file *os.File, downloaderWg *sync.WaitGroup) {
+		go func(i uint, dwLink string, rangeStart int64, rangeEnd int64, file *os.File, downloaderWg *sync.WaitGroup) {
 			response, err := getObjectRange(dwLink, rangeStart, rangeEnd)
 			if err != nil {
 				log.Fatalf("Request error in chunk: %d, Error: %s\n", i, err.Error())
